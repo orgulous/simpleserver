1bdd4d635230710e2beed90f648d50133de915b5
Luke Chen <lukechen@MacBook-Pro.local>
2022-12-30 17:10:22 -0600

    initial commit

---
+++ buttontest.js
@@ -0,0 +1,5 @@
+var formId = document.getElementById("button");
+function run(){
+  alert("This does not do anything yet"); 
+};
+formId.addEventListener("click", run, false);
---
+++ simpleserver.js
@@ -0,0 +1,101 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+var server = http.createServer(function (request, response) {
+    var path = url.parse(request.url).pathname;
+    switch (path) {
+        case "/buttontest.js":
+            fs.readFile(__dirname + path, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/javascript'
+                    });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        case "/page.css":
+            fs.readFile(__dirname + path, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/css'
+                    });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+
+        case '/':
+            fs.readFile(__dirname + '/HtmlPage1.html', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+            /* old code that just sends a simple message
+            response.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            response.write("This is Test Message. Hello everyone");
+            response.end();
+            break;
+            */
+        case '/HtmlPage1.html':
+            fs.readFile(__dirname + path, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        case '/HtmlPage2.html':
+            fs.readFile(__dirname + path, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
+
+});
+server.listen(8082);
