4262c602fe555a5454b1d3da7dff24dbae79e9ad
orgulous <lchenhw@gmail.com>
2023-06-01 20:52:59 -0500

    trying out express server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HtmlPage1.html'));
+});
+
+// Start the server
+app.listen(8082, () => {
+    console.log('Server is running on port 8082');
+});
---
+++ index.js
@@ -0,0 +1,34 @@
+// Fetch the CSV data and generate the cards
+fetch('test.csv')
+    .then(response => response.text())
+    .then(data => {
+        // Split the CSV data into rows
+        const rows = data.trim().split('\n');
+
+        // Iterate over each row and generate the cards
+        rows.forEach(row => {
+            // Split the row into columns
+            const [imageUrl, name, description, hyperlink] = row.split(';');
+
+            console.log(imageUrl);
+
+            // Create the card HTML
+            const cardHTML = `
+                <div class="col-md-3 d-flex">
+                    <div class="card card-container mb-5">
+                        <img src="${imageUrl}" class="card-img-top" alt="Card Image"> 
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">${description}</p>
+                            <p class="card-text"><a href="${hyperlink}">Link</a></p>
+                        </div>
+                    </div>
+                </div>
+
+            `;
+
+            // Append the card HTML to the cardRow element
+            document.getElementById('cardRow').innerHTML += cardHTML;
+        });
+    });
+
